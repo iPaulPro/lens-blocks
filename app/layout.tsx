21645adc81348a53489d46463f4c17b44b7922c9
Paul Burke <paul@paulburke.co>
2025-08-29 11:33:10 -0400

    chore: switches default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
           <Web3Provider>
             <SidebarProvider>
               <AppSidebar />
