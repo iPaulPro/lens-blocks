894206ef88b7cd1718c3f75b8a6ffe0ca05a679a
Paul Burke <paul@paulburke.co>
2025-09-04 19:04:02 -0400

    fix: connectkit reactive theme

--- app/web3-provider.tsx
+++ app/web3-provider.tsx
@@ -7,14 +7,17 @@ import { WagmiProvider } from "wagmi";
 import { config as wagmiConfig } from "@/lib/wagmi/config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider } from "connectkit";
+import { useTheme } from "next-themes";
 
 const queryClient = new QueryClient();
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
+  const { theme: nextTheme } = useTheme();
+
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider mode="light">
+        <ConnectKitProvider mode={nextTheme === "system" ? "auto" : (nextTheme as "light" | "dark")}>
           <LensProvider client={lensClient}>{children}</LensProvider>
         </ConnectKitProvider>
       </QueryClientProvider>
