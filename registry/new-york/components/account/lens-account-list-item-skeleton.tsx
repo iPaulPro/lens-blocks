cd306dc53ed8ec07e30764663f0db6129479f16a
Paul Burke <paul@paulburke.co>
2025-10-22 14:01:33 -0400

    fix: makes account list skeleton full width

--- registry/new-york/components/account/lens-account-list-item-skeleton.tsx
+++ registry/new-york/components/account/lens-account-list-item-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/registry/new-york/ui/skeleton";
 
 export const LensAccountListItemSkeleton = () => {
   return (
-    <div className="flex items-center space-x-2 p-2">
+    <div className="w-full flex items-center space-x-2 p-2">
       <Skeleton className="h-10 w-10 rounded-full flex-none" />
       <div className="flex-grow space-y-1.5">
         <Skeleton className="h-4 w-5/12" />
